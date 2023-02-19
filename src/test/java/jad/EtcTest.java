b60ca1ae974f146ced720bf6242d10ce108d2ef9
Russell Wallace <russell.wallace@gmail.com>
2023-02-19 12:33:23 +0000

    test classReader

--- src/test/java/jad/EtcTest.java
+++ src/test/java/jad/EtcTest.java
@@ -1,12 +1,62 @@
 package jad;
 
 import static org.junit.Assert.*;
+import static org.objectweb.asm.Opcodes.*;
 
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 import org.junit.Test;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.MethodVisitor;
 
 public class EtcTest {
+  static String name;
+  static String superName;
+  static final Set<String> methods = new HashSet<>();
+
+  static final class ClassPrinter extends ClassVisitor {
+    ClassPrinter() {
+      super(ASM7);
+    }
+
+    @Override
+    public void visit(
+        int version,
+        int access,
+        String name,
+        String signature,
+        String superName,
+        String[] interfaces) {
+      EtcTest.name = name;
+      EtcTest.superName = superName;
+    }
+
+    @Override
+    public MethodVisitor visitMethod(
+        int access, String name, String descriptor, String signature, String[] exceptions) {
+      methods.add(name);
+      return null;
+    }
+  }
+
   @Test
   public void ext() {
     assertEquals(Etc.ext("foo.txt"), "txt");
+  }
+
+  @Test
+  public void classReader() {
+    try {
+      var classReader = new ClassReader("java.lang.String");
+      var classPrinter = new ClassPrinter();
+      classReader.accept(classPrinter, 0);
+      assertEquals(name, "java/lang/String");
+      assertEquals(superName, "java/lang/Object");
+      assert methods.contains("equals");
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
